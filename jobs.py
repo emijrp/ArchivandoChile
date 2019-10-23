0e9d939f987e11ec417c09cf8ae4d40f18b8957f
tamycova <tcovacevich@uc.cl>
2019-10-23 17:12:47 -0300

    added archive_url to dump

--- jobs.py
+++ jobs.py
@@ -30,19 +30,22 @@ def main_job(context):
                 commented_retweet = tweets.get_commented_retweet(msg)
                 if msg.media is None and commented_retweet is not None:
                     msg = commented_retweet
-                # saving tweet for debugging
-                json.dump(msg.AsDict(), config.m)
-                config.m.flush()
+                msg_dict = msg.AsDict()
                 if str(msg.id) in config.config["cached"]:
+                    msg_dict["archive_url"] = "cached"
                     logging.info("tweet {} is cached! sending cached link".format(msg.id))
                     confirm_save(mention.id, config.config["cached"][str(msg.id)])
                 else:
                     tg = save_in_telegram(msg, context)
                     if tg is None:
+                        msg_dict["archive_url"] = None
                         logging.info("error saving in telegram")
                     else:
-                        pass
+                        msg_dict["archive_url"] = tg.link
                         confirm_save(mention.id, tg.link)
+                # saving tweet for debugging
+                json.dump(msg_dict, config.m)
+                config.m.flush()
             elif use is False:
                 confirm_error(mention.id,
                               "Hiciste muchos intentos en muy poco tiempo. "
